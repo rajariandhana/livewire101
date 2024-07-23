96122a21eb061f43506f731349732bc08dcfb957
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-07-23 21:10:14 +0700

    lupa

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App template</title>
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     {{-- @vite(['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js']) --}}
-    {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
-    {{-- <link rel="stylesheet" href="https://rsms.me/inter/inter.css"> --}}
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     {{-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> --}}
     {{-- @livewireStyles --}}
 </head>
